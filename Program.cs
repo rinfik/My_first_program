f95f1c4aaf3986489228b60ab34923f7bc2af8af
rinfik <114753250+rinfik@users.noreply.github.com>
2022-09-30 22:36:23 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Console.WriteLine("Привет! Напиши, пожалуйста, свои данные:");
+
+Console.WriteLine("Введите имя:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Ведите год рождения");
+string birth = Console.ReadLine();
+
+Console.WriteLine("Ввведите работу");
+string isEmployed = Console.ReadLine();
+
+Console.WriteLine("Введите вес");
+string weight = Console.ReadLine();
+
+int i1 = Convert.ToInt32 (birth);
+Console.WriteLine(DateTime.Today.Year);
+int currentyear = DateTime.Today.Year;
+int age = currentyear - i1;
+
+
+
+Console.WriteLine($"Имя: {name}");
+Console.WriteLine($"Возраст: {age}");
+Console.WriteLine($"Работает: {isEmployed}");
+Console.WriteLine($"Вес: {weight}");
---
+++ test.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class test
+    {
+/*
+        Console.WriteLine("Привет! Напиши, пожалуйста, свои данные:");
+
+        Console.WriteLine("Введите имя:");
+        //string name = "Jane";
+
+        string name = Console.ReadLine();
+
+                Console.WriteLine("Ведите дату рождения");
+int year = theDate.Year;
+        //string year = "Jane";
+        string x2 = ("Ведите дату рождения");
+        string z2 = int CurrentDay = DateTime.Today;
+string age = Console.ReadLine($"{x2} % {z2}");
+
+
+
+        Console.WriteLine("Ввведите работу");
+//bool isEmployed = true;
+string isEmployed = Console.ReadLine();
+
+        Console.WriteLine("Введите вес");
+//double weight = 64;
+string weight = Console.ReadLine();
+
+        Console.WriteLine($"Имя: {name}");
+Console.WriteLine($"Возраст: {age}");
+Console.WriteLine($"Работает: {isEmployed}");
+Console.WriteLine($"Вес: {weight}");
+*/
+    }
+}
